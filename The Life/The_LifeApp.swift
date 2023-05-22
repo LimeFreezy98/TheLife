15aed011032a4671be5faca06144978542162a2a
LimeFreezy98 <89162212+LimeFreezy98@users.noreply.github.com>
2023-05-22 09:16:45 -0600

    Initial Commit

---
+++ The Life/The_LifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_LifeApp.swift
+//  The Life
+//
+//  Created by Vasiliy on 5/22/23.
+//
+
+import SwiftUI
+
+@main
+struct The_LifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
